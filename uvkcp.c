d9b5f414bd45f67013d44ceee76a8131cf001b6e
tom zhou <appnet.link@gmail.com>
2020-06-05 09:09:29 +0800

    code cleanup

---
+++ uvkcp.c
@@ -0,0 +1,11 @@
+//////////////////////////////////////////////////////
+// KCP interfaces implementation
+// Copyright 2020, tom zhou<appnet.link@gmail.com>
+//////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include <assert.h>
+#include "kcp/ikcp.h"
+#include "uvkcp.h"
+
+//#define KCP_DEBUG 1
